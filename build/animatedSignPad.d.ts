923460d8de6e02465b7869d17120701bc433001b
PaulRichez <paul.richez59@gmail.com>
2022-02-26 09:21:39 +0100

    add type

---
+++ build/animatedSignPad.d.ts
@@ -0,0 +1,44 @@
+export interface IMouseCoord {
+    x: Number;
+    y: Number;
+}
+export declare type linecap = "butt" | "round" | "square";
+export interface IOptions {
+    lineWidth?: number;
+    strokeStyle?: String;
+    linecap?: linecap;
+}
+export interface IPaths {
+    pointPaths: String;
+    start: Date;
+    end: Date;
+}
+export declare class ASP {
+    canvas: HTMLCanvasElement;
+    options: IOptions;
+    paths: IPaths[];
+    private _ctx;
+    private _mouseCoord;
+    private drawer;
+    private stoper;
+    constructor(canvas: HTMLCanvasElement, options?: IOptions, paths?: IPaths[]);
+    private setDefaults;
+    private initEvent;
+    private removeEvent;
+    getCanvas(): any;
+    clearCanvas(): void;
+    start(event: MouseEvent): void;
+    setMouseCoord(event: MouseEvent): void;
+    stop(): void;
+    draw(event: MouseEvent): void;
+    getSvg(animation?: boolean): SVGSVGElement;
+    exportData(): {
+        options: IOptions;
+        paths: IPaths[];
+    };
+    importData(data: {
+        options: IOptions;
+        paths: IPaths[];
+    }): void;
+    private generateSvg;
+}
